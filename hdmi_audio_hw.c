71dd51091c7eaf2116de8d9faa9d320dd9b54890
ckkim <ckkim@hardkernel.com>
2014-11-24 18:41:41 +0900

    ODROIDC:audio: HDMI audio output default sampling rate 44.1Khz
    Change-Id: Ib1481d4f69c28057d371156bb83bcdee4166557a

--- hdmi_audio_hw.c
+++ hdmi_audio_hw.c
@@ -48,7 +48,7 @@
 /* ALSA cards for AML */
 #define CARD_AMLOGIC_BOARD 0 
 #define CARD_AMLOGIC_USB 1
-#define CARD_AMLOGIC_DEFAULT CARD_AMLOGIC_BOARD 
+#define CARD_AMLOGIC_DEFAULT CARD_AMLOGIC_USB 
 /* ALSA ports for AML */
 #define PORT_MM 1
 /* number of frames per period */
@@ -62,15 +62,14 @@ static unsigned  PERIOD_SIZE  = DEFAULT_PERIOD_SIZE;
 /* minimum sleep time in out_write() when write threshold is not reached */
 #define MIN_WRITE_SLEEP_US 5000
 
-#define RESAMPLER_BUFFER_FRAMES (PERIOD_SIZE * 6)
-#define RESAMPLER_BUFFER_SIZE (4 * RESAMPLER_BUFFER_FRAMES)
+#define RESAMPLER_BUFFER_SIZE 4096
 
-static unsigned int  DEFAULT_OUT_SAMPLING_RATE  = 48000;
+static unsigned int  DEFAULT_OUT_SAMPLING_RATE  = 44100;
 
 /* sampling rate when using MM low power port */
 #define MM_LOW_POWER_SAMPLING_RATE 44100
 /* sampling rate when using MM full power port */
-#define MM_FULL_POWER_SAMPLING_RATE 48000
+#define MM_FULL_POWER_SAMPLING_RATE 44100
 /* sampling rate when using VX port for narrow band */
 #define VX_NB_SAMPLING_RATE 8000
 
