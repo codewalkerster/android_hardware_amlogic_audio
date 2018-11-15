2fe51a5c436d8935ee8059e601bf5d9624d43004
jiejing.wang <jiejing.wang@amlogic.com>
2018-11-15 14:54:33 +0800

    audio: audio_hal: add license header [2/3]
    PD#SWPL-2111
    
    Problem:
    no license source files
    
    Solution:
    add license header
    
    Verify:
    verify by locally
    
    Change-Id: Ib67c73dd6c02e8fb3d2f07a78716bfb76f4dcf71
    Signed-off-by: jiejing.wang <jiejing.wang@amlogic.com>

--- utils/aml_data_utils.c
+++ utils/aml_data_utils.c
@@ -1,18 +1,23 @@
-/**
- ** aml_data_utils.c
- **
- ** This program is the factory of PCM data. Such as,
- **        re-mix
- **        extend
- **        extract
- **        exchange
- **        invert
- **        concat
- **        empty
- **        ditter
- **        replace
- ** author: shen pengru
+/*
+ * Copyright (C) 2018 Amlogic Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * DESCRIPTION:
+ *     This program is the factory of PCM data.
+ *
  */
+
 #define LOG_TAG "audio_data_utils"
 //#define LOG_NDEBUG 0
 
