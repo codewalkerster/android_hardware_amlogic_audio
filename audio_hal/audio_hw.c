3a0b7b2068674923a4c0f765b211a5e158083fbd
Chaomin Zheng <chaomin.zheng@amlogic.com>
2019-05-14 20:59:16 +0800

    Audio Hal: set adev patch src inval when release patch [1/2]
    PD# TV-5726
    
    Problem:
    mute does not work when switching from DTV to ATV
    
    Solution:
    set adev patch src inval when release patch
    
    Verify:
    verify by x301
    
    Change-Id: Ic5e52a15da2f4c4d1382eb93186c9f75d3ba82ba
    Signed-off-by: Chaomin Zheng <chaomin.zheng@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -9794,9 +9794,7 @@ static int adev_release_audio_patch(struct audio_hw_device *dev,
             release_patch(aml_dev);
         }
         aml_dev->audio_patching = 0;
-        if (aml_dev->active_inport != INPORT_TUNER) {
-            aml_dev->patch_src = SRC_INVAL;
-        }
+        aml_dev->patch_src = SRC_INVAL;
         aml_dev->parental_control_av_mute = false;
     }
 
@@ -9812,9 +9810,7 @@ static int adev_release_audio_patch(struct audio_hw_device *dev,
             release_dtv_patch(aml_dev);
         }
 #endif
-        if (aml_dev->active_inport != INPORT_TUNER) {
-            aml_dev->patch_src = SRC_INVAL;
-        }
+        aml_dev->patch_src = SRC_INVAL;
         if (aml_dev->patch_src == SRC_DTV) {
             ALOGI("patch src == DTV now line %d \n", __LINE__);
         }
