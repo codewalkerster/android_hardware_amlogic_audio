dcdc39dc39c56128279acea010060d88cb02a44c
hongchao.yin <hongchao.yin@amlogic.com>
2019-06-12 11:01:14 +0800

    audio_hal: add license declaration [1/3]
    PD#SWPL-9663
    
    Problem:
    clean audio no license file
    
    Solution:
    add license declaration
    
    Verify:
    verrify by marconi
    
    Change-Id: I7cd4d426fdf10b5f9ab1319994a633cb7c7d7b50
    Signed-off-by: hongchao.yin <hongchao.yin@amlogic.com>

--- utils/AQ_usb_bt/bt_usb_tool.cpp
+++ utils/AQ_usb_bt/bt_usb_tool.cpp
@@ -1,6 +1,12 @@
 /*
- This file is used for tuning audio.
- */
+* Copyright (c) 2014 Amlogic, Inc. All rights reserved.
+* *
+This source code is subject to the terms and conditions defined in the
+* file 'LICENSE' which is part of this source code package.
+* *
+Description:
+*/
+
 
 #include <errno.h>
 #include <stdio.h>
