49ac8478173b99b246967be2fb76c665c90be414
Chris KIM <codewalker@hardkernel.com>
2019-09-05 16:03:18 +0900

    ODROID-N2: Fix compile error.
    Change-Id: I00defb3172f5508b88bd5caf65a47e04cdc7f4f9
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>

--- audio_hal/audio_hw_profile.h
+++ audio_hal/audio_hw_profile.h
@@ -19,6 +19,8 @@
 #ifndef _AUDIO_HW_PROFILE_H_
 #define _AUDIO_HW_PROFILE_H_
 
+#include "audio_hw.h"
+
 int get_external_card(int type);
 char*  get_hdmi_sink_cap(const char *keys,audio_format_t format,struct aml_arc_hdmi_desc *p_hdmi_descs);
 char*  get_hdmi_arc_cap(unsigned *ad, int maxsize, const char *keys);
