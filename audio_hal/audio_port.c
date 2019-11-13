eb7edab5d3987ecfe68934522c50520005691612
Luke Go <sangch.go@gmail.com>
2019-11-13 17:23:14 +0900

    ODROID: audio: Apply dynamic audio output settings.
    - You can change audio output device dynamically via set
    'media.audio_hal.device' property.
    
    Signed-off-by: Luke Go <sangch.go@gmail.com>
    Change-Id: I3e396c9b2abac6851f218f0b46bfdca5534f5f1f

--- audio_hal/audio_port.c
+++ audio_hal/audio_port.c
@@ -28,6 +28,10 @@
 #include "aml_ringbuffer.h"
 #include "audio_hw_utils.h"
 #include "audio_hwsync.h"
+#if defined(ODROID)
+#include <aml_android_utils.h>
+#include "alsa_device_parser.h"
+#endif
 
 #define BUFF_CNT 4
 #define SYS_BUFF_CNT 4
@@ -486,6 +490,11 @@ static ssize_t output_port_start(struct output_port *port)
         ALOGE("%s(), unsupport", __func__);
         pcm_cfg.format = PCM_FORMAT_S16_LE;
     }
+
+#if defined(ODROID)
+    device = aml_getprop_int("media.audio_hal.device");
+    device = alsa_device_update_pcm_index(device, PLAYBACK);
+#endif
     ALOGI("%s(), open ALSA hw:%d,%d", __func__, card, device);
     pcm = pcm_open(card, device, PCM_OUT | PCM_MONOTONIC, &pcm_cfg);
     if ((pcm == NULL) || !pcm_is_ready(pcm)) {
